7f4ced50b836bde010fc61edf2f4abf5197dd39b
Daniel <halestorm102@gmail.com>
2017-09-26 10:07:06 -0700

    changes dated 9-25-17

---
+++ wp-content/themes/bb-theme-child/includes/copyright.php
@@ -0,0 +1,5 @@
+<div class="fl-page-footer-text fl-page-footer-text-1">
+	<span>Copyright &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> - All Rights Reserved</span>
+	<span> | </span>
+	<span id="fl-site-credits">Designed by <a href="http://www.qmdesigngroup.com/" target="_blank">QM Design Group</a>   |   Developed by <a href="http://www.wsiebiz.com" target="_blank">WSI</a></span>
+</div>
